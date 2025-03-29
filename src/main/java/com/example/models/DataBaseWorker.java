7332f7db9a347180d53ae0f530c1d25d6395c0c2
diman01012002@mail.ru <Zepbl4>
2025-03-29 12:35:55 +0300

    +dockerfile

--- src/main/java/com/example/models/DataBaseWorker.java
+++ src/main/java/com/example/models/DataBaseWorker.java
@@ -6,7 +6,7 @@ import java.sql.*;
 
 @Component
 public class DataBaseWorker {
-    private final String url = "jdbc:postgresql://postgresql:5432/mydatabase";
+    private final String url = "jdbc:postgresql://host.docker.internal:5432/mydatabase";
     private final String username = "admin";
     private final String password = "admin";
 
